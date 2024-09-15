87d4ead794aaf38504a9f3b518ea49b0a5bcffcb
hoangtd <hoangtd@solazu.com>
2024-09-15 17:15:26 +0700

    fix: remove file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login2 from './login2/login2';
 import Login3 from './login3/login3';
