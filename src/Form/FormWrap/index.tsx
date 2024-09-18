2bcce471caa02c678d0303145f52fb07409d9285
hoangtd <hoangtd@solazu.com>
2024-09-18 14:44:28 +0700

    feat: form submit

---
+++ src/Form/FormWrap/index.tsx
@@ -0,0 +1,22 @@
+import { Form, FormProps } from 'antd';
+import { ReactNode } from 'react';
+
+function FormWrap(props: FormProps & { children: ReactNode }) {
+  const formItemLayout =
+    props.layout === 'horizontal' || !props.layout ? { labelCol: { span: 4 }, wrapperCol: { span: 14 } } : null;
+
+  return (
+    <Form
+      {...formItemLayout}
+      layout={props.layout || 'horizontal'}
+      form={props.form}
+      {...props}
+      initialValues={props.initialValues}
+      scrollToFirstError={true}
+    >
+      {props.children}
+    </Form>
+  );
+}
+
+export default FormWrap;
