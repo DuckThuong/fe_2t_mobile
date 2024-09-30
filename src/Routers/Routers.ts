1274590461be01511a3deb822507e9caf60b186c
hoangtd <hoangtd@solazu.com>
2024-09-30 17:26:30 +0700

    fix content

--- src/Routers/Routers.ts
+++ src/Routers/Routers.ts
@@ -1,11 +1,11 @@
-const CUSTOMER = ""; //đường dân của sever: localhost:3000
+const CUSTOMER = "";
 export const ACTION = {
-  CREATE: "create", //đường dẫn cho tạo màn mới
+  CREATE: "create",
 
-  DETAIL: "detail", //đường dẫn cho việc xem màn vừa tạo mới
+  DETAIL: "detail",
 };
 export const CUSTOMER_ROUTE_NAME = {
-  //customer router name: tên của các màn mà trang web muốn hướng đến
+  //Log_In
   LOG_IN: "",
   //Sign_in
   EMAIL_INPUT: "email_input",
@@ -18,7 +18,7 @@ export const CUSTOMER_ROUTE_NAME = {
   FORGOT_SUCCESS: "forgot-success",
 };
 export const CUSTOMER_ROUTER_PATH = {
-  //gộp 2 cái trên lại thì ra cục này, đường dẫn hoàn chỉnh: Customer path
+  //Log_In
   LOG_IN: `${CUSTOMER}/${CUSTOMER_ROUTE_NAME.LOG_IN}`,
   //Sign_in
   EMAIL_INPUT: `${CUSTOMER}/${CUSTOMER_ROUTE_NAME.EMAIL_INPUT}`,
--- src/SuspenseWrapper/index.tsx
+++ src/SuspenseWrapper/index.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { Suspense } from "react";
 
 export const SuspenseWrapper = (props) => {
-  //xử lý trạng thái loading cho trang web
   return (
     <Suspense
       key={"suspense-" + random(10)}
@@ -12,11 +11,9 @@ export const SuspenseWrapper = (props) => {
         <div className="w-100 h-100 d-flex justify-content-center align-items-center">
           <Spin size="large" />
         </div>
-        //mạng lag thì nó chạy vào cái cục fallback
       }
     >
-      {props.component} {/* không lag thì nó chạy vào dòng này */}
-      
+      {props.component}
     </Suspense>
   );
 };
