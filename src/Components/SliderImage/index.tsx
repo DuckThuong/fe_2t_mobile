656eaddddad9d3ce96b0b297cbf0b9bdf2dd909e
thuongtrinhduc <thuongtrinhduc@tsoft.vn>
2024-12-18 09:48:50 +0700

    feat: header

--- src/Components/SliderImage/index.tsx
+++ src/Components/SliderImage/index.tsx
@@ -1,36 +1,15 @@
+import { Carousel, Image } from "antd";
 import React from "react";
-import Slider from "react-slick";
-import { Card, Image } from "antd";
 
 interface Props {
-  image: string[];
+  image?: string[];
 }
-const ImageCarousel: React.FC<Props> = ({ image }) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
-
-  return (
-    <Card title="Thẻ ảnh cuộn tự động">
-      <Slider {...settings}>
-        {image.map((src, index) => (
-          <div key={index}>
-            <Image
-              src={src}
-              alt={`Slide ${index + 1}`}
-              style={{ width: "100%", borderRadius: "8px" }}
-            />
-          </div>
-        ))}
-      </Slider>
-    </Card>
-  );
-};
+const ImageCarousel: React.FC<Props> = ({ image }) => (
+  <Carousel className="image-carousel" autoplay>
+    {image?.map((img, index) => (
+      <Image className="image-carousel_item" key={index} src={img} />
+    ))}
+  </Carousel>
+);
 
 export default ImageCarousel;
--- src/Layout/TrangChu/HomeHeader/index.tsx
+++ src/Layout/TrangChu/HomeHeader/index.tsx
@@ -1,11 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import ImageCarousel from "../../../Components/SliderImage";
+import { QUERY_KEY } from "../../../configs/apiConfig";
+import { imageApi } from "../../../api/api";
+import "../style.scss";
 
 export const HomeHeader = () => {
-  const image = [];
+  const { data: imageData } = useQuery({
+    queryKey: [QUERY_KEY.GET_USER],
+    queryFn: imageApi.getAllImage,
+  });
+  const imageUrls = imageData?.ImageList?.map((image) => image.ImageURL);
   return (
     <div className="home-header">
       <div className="home-header_image">
-        <ImageCarousel image={image} />
+        <ImageCarousel image={imageUrls} />
       </div>
     </div>
   );
