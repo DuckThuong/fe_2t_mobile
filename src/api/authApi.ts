88bee5a073edf3003a02e6c6966ea3d074d85a9c
vynguyenle <vylenguyen@tcom.vn>
2025-05-01 05:04:23 +0700

    fix: login

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,22 +1,24 @@
 import axios from "axios";
 import { QUERY_KEY } from "./apiConfig";
+import { useNavigate } from "react-router-dom";
+import { CUSTOMER_ROUTER_PATH } from "../Routers/Routers";
 
 interface LoginResponse {
   token: string;
   user: any;
 }
 
-export const login = async (email: string, password: string) => {
+export const login = async (PhoneNumber: string, Password: string) => {
   try {
     const response = await axios.post<LoginResponse>(
-      QUERY_KEY.GET_USER + "/login",
+      QUERY_KEY.GET_USER + "/log-in",
       {
-        email,
-        password,
+        PhoneNumber,
+        Password,
       }
     );
     if (response.data.token) {
-      localStorage.setItem("accessToken", response.data.token);
+      localStorage.setItem("token", response.data.token);
       localStorage.setItem("user", JSON.stringify(response.data.user));
       return response.data;
     } else {
